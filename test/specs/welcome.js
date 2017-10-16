62f66a503a3b1e1753c2573a8d2d5d5393c843e6
Marius <marius@cozycloud.cc>
2017-10-16 16:08:23 +0200

    tests: check if onboarding is displayed

--- test/specs/welcome.js
+++ test/specs/welcome.js
@@ -14,40 +14,29 @@ const fetchRegisterToken = () => fetch(
       'http://localhost:5984/global%2Finstances/_all_docs?include_docs=true'
     )
     .then(response => response.json())
-    .then(json => {
-      return filterInstance(json.rows, 'cozy.local:8080')
-    })
-    .then(instance => {
-      return fetch(`http://localhost:5984/global%2Finstances/${instance.id}`)
-    })
+    .then(json => filterInstance(json.rows, 'cozy.tools:8080'))
+    .then(instance => fetch(
+      `http://localhost:5984/global%2Finstances/${instance.id}`
+    ))
     .then(response => response.json())
-    .then(json => {
-      return json['register_token']
-    })
+    .then(json => json['register_token'])
     .then(registerTokenBase64 => Buffer.from(registerTokenBase64, 'base64').toString('hex'))
 
 describe('BrowserStack Local Testing', function () {
   it('can check tunnel working', function (done) {
     console.log('can check tunnel working')
+    let token = null
+    fetchRegisterToken()
+      .then(registerToken => token = registerToken)
+
+    // Wait until the fetchRegisterToken Promise is resolved, then set token
+    console.log(`accessing http://onboarding.cozy.tools:8080/?registerToken=${token}`)
     browser
-      .url(`http://cozy.tools:8080/`)
-    assert.equal(browser.getText('h1'), 'Welcome cozy!')
-    // fetchRegisterToken()
-    //   .then(registerToken => {
-    //     console.log(`accessing http://onboarding.cozy.local:8080/?registerToken=${registerToken}`)
-    //     browser
-    //       .url(`http://onboarding.cozy.local:8080/?registerToken=${registerToken}`)
-    //
-    //     console.log('url.then')
-    //     browser.waitUntil(() => {
-    //       console.log('browser.waitUntil', browser.getUrl())
-    //       return browser.getUrl() !== `http://onboarding.cozy.local:8080/?registerToken=${registerToken}`
-    //     }, 30000)
-    //     .then(() => {
-    //       // assert(browser.getSource().match(/Up and running/i))
-    //       assert.equal(browser.getText('h1'), 'Plouf!')
-    //       done()
-    //     })
-    //   })
+      .waitUntil(() => token !== null)
+    browser
+      .url(`http://onboarding.cozy.tools:8080/?registerToken=${token}`)
+      .pause(5000)
+
+    assert.equal(browser.getText('h1'), 'Choose your password')
   })
 })
